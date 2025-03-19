3ef41e1aba20d29b0960f6c23c1cb826f1390397
Pablo Gonzalez Moncalvillo <pablo@theticketmerchant.com.au>
2025-03-19 22:46:40 +0100

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import CreateRoomScreen from "./screens/CreateRoomScreen";
+import HomeScreen from "./screens/HomeScreen";
+import RoomScreen from "./screens/RoomScreen";
+export type RootStackParamList = {
+  Home: undefined;
+  CreateRoom: undefined;
+  Room: { roomCode?: string };
+};
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export default function App() {
+function RootStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="CreateRoom" component={CreateRoomScreen} />
+      <Stack.Screen name="Room" component={RoomScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
